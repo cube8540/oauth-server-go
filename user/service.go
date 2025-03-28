f03d7cb5de7d471bb071d2944e92ca39679e8a0f
cube8540 <cube8540@gmail.com>
2025-03-28 21:32:59 +0900

    엔티티, 도메인 로직 삭제

--- user/service.go
+++ user/service.go
@@ -3,6 +3,9 @@ package user
 import (
 	"errors"
 	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+	"oauth-server-go/conf"
 )
 
 var (
@@ -11,6 +14,36 @@ var (
 	ErrPasswordNotMatch     = errors.New("password does not match")
 	ErrAccountLocked        = errors.New("account is locked")
 )
+
+// Hasher 문자열을 받아 해싱하여 반환하는 함수를 정의한 인터페이스
+type Hasher interface {
+	Hashing(v string) (string, error)
+	Compare(hashed, cmp string) (bool, error)
+}
+
+// BcryptHasher Bcrypt 해싱
+type BcryptHasher struct{}
+
+func NewBcryptHasher() BcryptHasher {
+	return BcryptHasher{}
+}
+
+func (_ BcryptHasher) Hashing(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
+
+func (_ BcryptHasher) Compare(hashed, password string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(hashed), []byte(password))
+	if err != nil {
+		return false, err
+	} else {
+		return true, nil
+	}
+}
 
 type (
 	LoginModel struct {
@@ -28,18 +61,19 @@ func Login(req *LoginRequest, hasher Hasher) (*LoginModel, error) {
 		return nil, ErrRequireParamsMissing
 	}
 
-	account, err := FindAccountByUsername(req.Username)
-	if err != nil {
-		return nil, fmt.Errorf("error by repository %w", err)
-	} else if account == nil {
+	var account Account
+	err := conf.GetDB().Where(&Account{Username: req.Username}).First(&account).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
 		return nil, ErrAccountNotFound
+	} else if err != nil {
+		return nil, fmt.Errorf("%w", err)
 	}
 
-	if cmp, err := account.PasswordCompare(hasher, req.Password); err != nil {
+	if cmp, err := hasher.Compare(account.Password, req.Password); err != nil {
 		return nil, err
 	} else if !cmp {
 		return nil, ErrPasswordNotMatch
-	} else if !account.active {
+	} else if !account.Active {
 		return nil, ErrAccountLocked
 	}
 
