c1fd23891b9e814218d6c6d57c92e8b77714e01f
cube8540 <cube8540@gmail.com>
2025-04-17 12:06:16 +0900

    테스트 결과 확인 방식 변경/ 예상 되는 토큰/리플레시 토큰 함수화

--- oauth/token/service_test.go
+++ oauth/token/service_test.go
@@ -18,6 +18,7 @@ type mockStore struct {
 	savedToken   []*Token
 	savedRefresh []*RefreshToken
 
+	deletedToken   []*Token
 	deletedRefresh []*RefreshToken
 }
 
@@ -39,6 +40,7 @@ func (m *mockStore) FindRefreshTokenByValue(v string) (*RefreshToken, error) {
 
 func (m *mockStore) Refresh(oldRefreshToken *RefreshToken, newToken *Token, fn func(t *Token) *RefreshToken) error {
 	m.deletedRefresh = append(m.deletedRefresh, oldRefreshToken)
+	m.deletedToken = append(m.deletedToken, oldRefreshToken.Token)
 	m.savedToken = append(m.savedToken, newToken)
 	if refresh := fn(newToken); refresh != nil {
 		m.savedRefresh = append(m.savedRefresh, refresh)
@@ -55,126 +57,151 @@ func authorizationCodeConsumer(then string, rtn *code.AuthorizationCode) AuthCod
 	}
 }
 
-type authCodeFlowTestCase struct {
-	name     string
-	store    *mockStore
-	consumer AuthCodeConsume
-	gen      IDGenerator
-	c        *client.Client
-	r        *pkg.TokenRequest
-	expect   authCodeFlowExpect
+func scopeList(c ...string) []client.Scope {
+	var scopes []client.Scope
+	for _, v := range c {
+		scopes = append(scopes, client.Scope{Code: v})
+	}
+	return scopes
 }
 
-type authCodeFlowExpect struct {
-	token        func(t *Token) bool
-	refresh      func(t *RefreshToken) bool
-	savedToken   func(t *Token) bool
-	savedRefresh func(t *RefreshToken) bool
-	err          error
+func expectToken(id string, c uint, scopes []client.Scope) func(t *Token) bool {
+	return func(t *Token) bool {
+		return t.Value == id && t.ClientID == c && slices.EqualFunc(t.Scopes, scopes, func(s1, s2 client.Scope) bool {
+			return s1.Code == s2.Code
+		})
+	}
+}
+
+func expectRefresh(id, token string) func(t *RefreshToken) bool {
+	return func(t *RefreshToken) bool {
+		return t.Value == id && t.Token.Value == token
+	}
+}
+
+type tokenGrantedExpect struct {
+	token         func(t *Token) bool
+	refresh       func(t *RefreshToken) bool
+	savedToken    func(t *Token) bool
+	savedRefresh  func(t *RefreshToken) bool
+	deleteToken   func(t *Token) bool
+	deleteRefresh func(t *RefreshToken) bool
+	err           error
+}
+
+type tokenGrantTestCase struct {
+	name   string
+	store  *mockStore
+	c      *client.Client
+	r      *pkg.TokenRequest
+	expect tokenGrantedExpect
+}
+
+type authCodeFlowTestCase struct {
+	tokenGrantTestCase
+	consumer AuthCodeConsume
+	gen      IDGenerator
 }
 
 func TestAuthorizationCodeFlow_Generate(t *testing.T) {
 	tests := []authCodeFlowTestCase{
 		{
-			name: "인가 코드의 클라이언트와 요청 클라이언트가 다른 경우",
+			tokenGrantTestCase: tokenGrantTestCase{
+				name: "인가 코드의 클라이언트와 요청 클라이언트가 다른 경우",
+				c:    &client.Client{ID: 1},
+				r: &pkg.TokenRequest{
+					Code: "code",
+				},
+				expect: tokenGrantedExpect{
+					err: ErrUnauthorized,
+				},
+			},
 			consumer: authorizationCodeConsumer("code", &code.AuthorizationCode{
 				ClientID: 2,
 			}),
-			c: &client.Client{ID: 1},
-			r: &pkg.TokenRequest{
-				Code: "code",
-			},
-			expect: authCodeFlowExpect{
-				err: ErrUnauthorized,
-			},
 		},
 		{
-			name: "인가 코드가 만료 되었을 경우",
+			tokenGrantTestCase: tokenGrantTestCase{
+				name:   "인가 코드가 만료 되었을 경우",
+				c:      &client.Client{ID: 1},
+				r:      &pkg.TokenRequest{Code: "code"},
+				expect: tokenGrantedExpect{err: ErrTokenCannotGrant},
+			},
 			consumer: authorizationCodeConsumer("code", &code.AuthorizationCode{
 				ClientID:  1,
 				ExpiredAt: time.Now().Add(-1 * time.Second),
 			}),
-			c:      &client.Client{ID: 1},
-			r:      &pkg.TokenRequest{Code: "code"},
-			expect: authCodeFlowExpect{err: ErrTokenCannotGrant},
 		},
 		{
-			name: "PKCE 검증에 실패 했을 경우",
-			consumer: authorizationCodeConsumer("code", &code.AuthorizationCode{
-				ClientID:            1,
-				ExpiredAt:           time.Now().Add(1 * time.Second),
-				CodeChallengeMethod: pkg.ChallengePlan,
-				CodeChallenge:       "abcde",
-			}),
-			c: &client.Client{ID: 1},
-			r: &pkg.TokenRequest{
-				Code:         "code",
-				CodeVerifier: "wrong verifier",
+			tokenGrantTestCase: tokenGrantTestCase{
+				name: "PKCE 검증에 실패 했을 경우",
+				c:    &client.Client{ID: 1},
+				r: &pkg.TokenRequest{
+					Code:         "code",
+					CodeVerifier: "wrong verifier",
+				},
+				expect: tokenGrantedExpect{err: ErrInvalidRequest},
 			},
-			expect: authCodeFlowExpect{err: ErrInvalidRequest},
-		},
-		{
-			name: "요청 클라이언트가 공개 클라이언트인 경우",
 			consumer: authorizationCodeConsumer("code", &code.AuthorizationCode{
 				ClientID:            1,
 				ExpiredAt:           time.Now().Add(1 * time.Second),
 				CodeChallengeMethod: pkg.ChallengePlan,
 				CodeChallenge:       "abcde",
 			}),
-			store: &mockStore{},
+		},
+		{
+			tokenGrantTestCase: tokenGrantTestCase{
+				name:  "요청 클라이언트가 공개 클라이언트인 경우",
+				store: &mockStore{},
+				c: &client.Client{
+					ID:   1,
+					Type: pkg.ClientTypePublic,
+				},
+				r: &pkg.TokenRequest{
+					Code:         "code",
+					CodeVerifier: "abcde",
+				},
+				expect: tokenGrantedExpect{
+					savedToken: expectToken("TEST_TOKEN_ID", 1, scopeList("scope_1", "scope_2", "scope_3")),
+				},
+			},
+			consumer: authorizationCodeConsumer("code", &code.AuthorizationCode{
+				ClientID:            1,
+				ExpiredAt:           time.Now().Add(1 * time.Second),
+				CodeChallengeMethod: pkg.ChallengePlan,
+				CodeChallenge:       "abcde",
+				Scopes:              scopeList("scope_1", "scope_2", "scope_3"),
+			}),
 			gen: func() string {
 				return "TEST_TOKEN_ID"
 			},
-			c: &client.Client{
-				ID:   1,
-				Type: pkg.ClientTypePublic,
-			},
-			r: &pkg.TokenRequest{
-				Code:         "code",
-				CodeVerifier: "abcde",
-			},
-			expect: authCodeFlowExpect{
-				token: func(t *Token) bool {
-					return t.Value == "TEST_TOKEN_ID" && t.ClientID == 1
-				},
-				savedToken: func(t *Token) bool {
-					return t.Value == "TEST_TOKEN_ID" && t.ClientID == 1
-				},
-			},
 		},
 		{
-			name: "요청 클라이언트가 공개 클라이언트가 아닌 경우",
+			tokenGrantTestCase: tokenGrantTestCase{
+				name:  "요청 클라이언트가 공개 클라이언트가 아닌 경우",
+				store: &mockStore{},
+				c: &client.Client{
+					ID:   1,
+					Type: pkg.ClientTypeConfidential,
+				},
+				r: &pkg.TokenRequest{
+					Code:         "code",
+					CodeVerifier: "abcde",
+				},
+				expect: tokenGrantedExpect{
+					savedToken:   expectToken("TEST_TOKEN_ID", 1, scopeList("scope_1", "scope_2", "scope_3")),
+					savedRefresh: expectRefresh("TEST_TOKEN_ID", "TEST_TOKEN_ID"),
+				},
+			},
 			consumer: authorizationCodeConsumer("code", &code.AuthorizationCode{
 				ClientID:            1,
 				ExpiredAt:           time.Now().Add(1 * time.Second),
 				CodeChallengeMethod: pkg.ChallengePlan,
 				CodeChallenge:       "abcde",
+				Scopes:              scopeList("scope_1", "scope_2", "scope_3"),
 			}),
-			store: &mockStore{},
 			gen: func() string {
 				return "TEST_TOKEN_ID"
-			},
-			c: &client.Client{
-				ID:   1,
-				Type: pkg.ClientTypeConfidential,
-			},
-			r: &pkg.TokenRequest{
-				Code:         "code",
-				CodeVerifier: "abcde",
-			},
-			expect: authCodeFlowExpect{
-				token: func(t *Token) bool {
-					return t.Value == "TEST_TOKEN_ID" && t.ClientID == 1
-				},
-				savedToken: func(t *Token) bool {
-					return t.Value == "TEST_TOKEN_ID" && t.ClientID == 1
-				},
-				refresh: func(t *RefreshToken) bool {
-					return t.Value == "TEST_TOKEN_ID" && t.Token.Value == "TEST_TOKEN_ID"
-				},
-				savedRefresh: func(t *RefreshToken) bool {
-					return t.Value == "TEST_TOKEN_ID" && t.Token.Value == "TEST_TOKEN_ID"
-				},
 			},
 		},
 	}
@@ -184,13 +211,7 @@ func TestAuthorizationCodeFlow_Generate(t *testing.T) {
 			srv := NewAuthorizationCodeFlow(tc.store, tc.consumer)
 			srv.IDGenerator = tc.gen
 
-			token, refresh, err := srv.Generate(tc.c, tc.r)
-			if token != nil && !tc.expect.token(token) {
-				t.Errorf("반환 받은 토큰이 기대 하였던 것과 다릅니다.\n 실제:%+#v", token)
-			}
-			if refresh != nil && !tc.expect.refresh(refresh) {
-				t.Errorf("반환 받은 리플레시 토큰이 기대 하였던 것과 다릅니다.\n실제:%+#v", refresh)
-			}
+			_, _, err := srv.Generate(tc.c, tc.r)
 			if !errors.Is(err, tc.expect.err) {
 				t.Errorf("발생한 에러가 기대 하였던 것과 다릅니다.\n기대:%v\n실제:%v", tc.expect.err, err)
 			}
