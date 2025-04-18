35bc2860ca9f055d43e1cd018570ded11c241659
cube8540 <cube8540@gmail.com>
2025-04-18 13:47:02 +0900

    테스트 방식을 테이블 방식으로 변경

--- oauth/handler_test.go
+++ oauth/handler_test.go
@@ -2,7 +2,6 @@ package oauth
 
 import (
 	"encoding/json"
-	"errors"
 	"github.com/gin-contrib/sessions"
 	"github.com/stretchr/testify/assert"
 	"net/url"
@@ -21,87 +20,135 @@ func fixedClientRetriever(id string, c *client.Client) func(id string) (*client.
 	}
 }
 
-func TestHandler_authorize(t *testing.T) {
-	handler := h{
-		clientRetriever: fixedClientRetriever(testClientIDValue, testClient),
-	}
-	t.Run("client_id가 입력되지 않았을 경우 ErrInvalidRequest 발생", func(t *testing.T) {
-		query := url.Values{
-			"client_id": nil,
-		}
-
-		c, _, _ := testutils.MockGin(query, nil)
-		err := handler.authorize(c)
-
-		var oauthErr *Error
-		if errors.As(err, &oauthErr) {
-			assert.Equal(t, oauthErr.Code, pkg.ErrInvalidRequest)
-		} else {
-			t.Errorf("oauth.Error이 발생해야 합니다. actual: %v", err)
-		}
-	})
-	t.Run("response_type이 입력되지 않았을 경우 ErrInvalidRequest 발생", func(t *testing.T) {
-		query := url.Values{
-			"client_id":     []string{testClientIDValue},
-			"response_type": nil,
-			"redirect_uri":  []string{testLocalHost8080},
-		}
-
-		c, _, _ := testutils.MockGin(query, nil)
-		err := handler.authorize(c)
-		assertWrapError(t, err, pkg.ErrInvalidRequest, testutils.ParseURL(testLocalHost8080))
-	})
-	t.Run("response_type이 code, token이 아닌 경우 ErrUnsupportedResponseType 발생", func(t *testing.T) {
-		query := url.Values{
-			"client_id":     []string{testClientIDValue},
-			"response_type": []string{"wrong_type"},
-			"redirect_uri":  []string{testLocalHost8080},
-		}
-
-		c, _, _ := testutils.MockGin(query, nil)
-		err := handler.authorize(c)
-		assertWrapError(t, err, pkg.ErrUnsupportedResponseType, testutils.ParseURL(testLocalHost8080))
-	})
-	t.Run("세션에 요청 정보를 저장", func(t *testing.T) {
-		query := url.Values{
-			"client_id":     []string{testClientIDValue},
-			"response_type": []string{string(pkg.ResponseTypeCode)},
-			"redirect_uri":  []string{testLocalHost8080},
-			"scope":         []string{"scope_1 scope_2"},
-		}
-
-		c, _, engine := testutils.MockGin(query, nil)
-		engine.HTMLRender = testutils.NewHTMLRender()
-
-		s := testutils.NewSessions(testSessionID)
-		c.Set(sessions.DefaultKey, s)
-
-		_ = handler.authorize(c)
-
-		origin := &pkg.AuthorizationRequest{
-			ClientID:     testClientIDValue,
-			Redirect:     testLocalHost8080,
-			Scopes:       "scope_1 scope_2",
-			ResponseType: pkg.ResponseTypeCode,
-		}
-		expect, _ := json.Marshal(origin)
-		saved := s.Get(sessionKeyOriginAuthRequest)
-		assert.Equal(t, saved, expect)
-	})
+type requestTestCase struct {
+	name            string
+	query           url.Values
+	clientRetriever func(id string) (*client.Client, error)
 }
 
-func assertWrapError(t *testing.T, err error, code string, to *url.URL) {
-	var route *routeErr
-	if errors.As(err, &route) {
-		assert.Equal(t, route.to, to)
+type handlerAuthorizeExpect struct {
+	oauthError *Error
+	routeError *routeErr
 
-		var oauthErr *Error
-		if errors.As(err, &oauthErr) {
-			assert.Equal(t, oauthErr.Code, code)
-		} else {
-			t.Errorf("oauth.Error가 발생해야 합니다. actual: %v", err)
-		}
-	} else {
-		t.Errorf("oauth.routeError가 발생해야 합니다. actual: %v", err)
+	savedRequestIntoSession *pkg.AuthorizationRequest
+}
+
+type handlerAuthorizeTestCase struct {
+	requestTestCase
+	session  sessions.Session
+	expected handlerAuthorizeExpect
+}
+
+func TestHandler_authorize(t *testing.T) {
+	tests := []handlerAuthorizeTestCase{
+		{
+			requestTestCase: requestTestCase{
+				name: "client_id가 입력 되지 않은 경우 ErrInvalidRequest 발생",
+				query: map[string][]string{
+					"client_id": nil,
+				},
+			},
+			expected: handlerAuthorizeExpect{
+				oauthError: &Error{
+					Code: pkg.ErrInvalidRequest,
+				},
+			},
+		},
+		{
+			requestTestCase: requestTestCase{
+				name: "response_type이 입력 되지 않은 경우 ErrInvalidRequest 발생",
+				query: map[string][]string{
+					"client_id":     {testClientIDValue},
+					"response_type": nil,
+					"redirect_uri":  {testLocalHost8080},
+				},
+				clientRetriever: fixedClientRetriever(testClientIDValue, testClient),
+			},
+			expected: handlerAuthorizeExpect{
+				oauthError: &Error{
+					Code: pkg.ErrInvalidRequest,
+				},
+				routeError: &routeErr{
+					to: testutils.ParseURL(testLocalHost8080),
+				},
+			},
+		},
+		{
+			requestTestCase: requestTestCase{
+				name: "response_type이 code, token이 아닌 경우 ErrUnsupportedResponseType 발생",
+				query: map[string][]string{
+					"client_id":     {testClientIDValue},
+					"response_type": {"wrong_type"},
+					"redirect_uri":  {testLocalHost8080},
+				},
+				clientRetriever: fixedClientRetriever(testClientIDValue, testClient),
+			},
+			expected: handlerAuthorizeExpect{
+				oauthError: &Error{
+					Code: pkg.ErrUnsupportedResponseType,
+				},
+				routeError: &routeErr{
+					to: testutils.ParseURL(testLocalHost8080),
+				},
+			},
+		},
+		{
+			requestTestCase: requestTestCase{
+				name: "세션에 요청 정보를 저장",
+				query: map[string][]string{
+					"client_id":     {testClientIDValue},
+					"response_type": {string(pkg.ResponseTypeCode)},
+					"redirect_uri":  {testLocalHost8080},
+					"scope":         {"scope_1 scope_2"},
+				},
+				clientRetriever: fixedClientRetriever(testClientIDValue, testClient),
+			},
+			session: testutils.NewSessions(testSessionID),
+			expected: handlerAuthorizeExpect{
+				savedRequestIntoSession: &pkg.AuthorizationRequest{
+					ClientID:     testClientIDValue,
+					ResponseType: pkg.ResponseTypeCode,
+					Redirect:     testLocalHost8080,
+					Scopes:       "scope_1 scope_2",
+				},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			handler := h{
+				clientRetriever: tc.clientRetriever,
+			}
+			c, _, engine := testutils.MockGin(tc.query, nil)
+
+			engine.HTMLRender = testutils.NewHTMLRender()
+			if tc.session != nil {
+				c.Set(sessions.DefaultKey, tc.session)
+			}
+
+			err := handler.authorize(c)
+			if tc.expected.oauthError != nil {
+				var oauthError *Error
+				assert.ErrorAs(t, err, &oauthError)
+				assert.Equal(t, tc.expected.oauthError.Code, oauthError.Code)
+			}
+			if tc.expected.routeError != nil {
+				var routeError *routeErr
+				assert.ErrorAs(t, err, &routeError)
+				assert.Equal(t, tc.expected.routeError.to, routeError.to)
+			}
+			if tc.expected.oauthError == nil && tc.expected.routeError == nil {
+				assert.Nil(t, err)
+			}
+			if tc.expected.savedRequestIntoSession != nil {
+				serial := sessions.Default(c).Get(sessionKeyOriginAuthRequest)
+
+				var savedAuthorizationRequest pkg.AuthorizationRequest
+				_ = json.Unmarshal(serial.([]byte), &savedAuthorizationRequest)
+
+				assert.Equal(t, *tc.expected.savedRequestIntoSession, savedAuthorizationRequest)
+			}
+		})
 	}
 }
